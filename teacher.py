0195c89b367a7eed36b12dfcc03538e00d5558f1
arbor <119795609+cooingcoding@users.noreply.github.com>
2023-01-03 23:25:55 +0900

    수업시간 복습하기

---
+++ teacher.py
@@ -0,0 +1,4 @@
+def pprint(arr):
+    print('type : {}'.format(type(arr)))
+    print('shape: {}, dimension:{}, dtype:{}'.format(arr.shape, arr.ndim, arr.dtype))
+    print("Array's Data : \n", arr)
